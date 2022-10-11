a44494ffe691095fa5098f87bb3084530e5a662e
xueyong <1085523029@qq.com>
2022-10-11 18:32:33 +0800

    :ambulance: set cookie add domian

--- apps/web/pages/api/auth/[...supabase].ts
+++ apps/web/pages/api/auth/[...supabase].ts
@@ -2,5 +2,5 @@ import { handleAuth } from "@supabase/auth-helpers-nextjs"
 
 export default handleAuth({
   logout: { returnTo: "/" },
-  cookieOptions: { lifetime: 1 * 365 * 24 * 60 * 60 }, // Keep the user logged in for a year.
+  cookieOptions: { lifetime: 1 * 365 * 24 * 60 * 60, domain: "daidai.cyou" }, // Keep the user logged in for a year.
 })
