bcc1f73d46e178686f825e1c00c8b8fc3ed6e6c2
xueyong <1085523029@qq.com>
2022-10-12 23:46:52 +0800

    :construction: misc

--- packages/supabase-auth-helpers-shared/src/index.ts
+++ packages/supabase-auth-helpers-shared/src/index.ts
@@ -4,7 +4,7 @@ import {
 } from "@supabase/auth-helpers-react"
 import { UserProvider as ExtensionUserProvider, useUser as extensionUseUser } from "./extension"
 
-const isExtension = process.env.NEXT_PBULIC_EXTENSION === "chrome"
+const isExtension = process.env.NEXT_PUBLIC_EXTENSION === "chrome"
 
 export const UserProvider = isExtension ? ExtensionUserProvider : WebUserProvider
 export const useUser = isExtension ? extensionUseUser : webUseUser
