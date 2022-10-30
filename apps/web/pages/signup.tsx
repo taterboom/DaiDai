92f5189308f41db401a41f6b307ce27562f27fda
xueyong <1085523029@qq.com>
2022-10-30 15:05:55 +0800

    💄 add cat model

--- apps/web/pages/signup.tsx
+++ apps/web/pages/signup.tsx
@@ -6,7 +6,7 @@ const DaidaiModel = dynamic(() => import("../components/DaidaiModel"), { ssr: fa
 const Signup = () => {
   return (
     <div className="flex items-center flex-col pt-32">
-      <div className="w-[200px] h-[200px]">
+      <div className="w-[256px] h-[256px]">
         <DaidaiModel />
       </div>
       <Auth type="signup" />
